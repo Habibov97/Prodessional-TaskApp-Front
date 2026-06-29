7a5ccb68fcb9c7d5b5a8f457fb640d21d1a77d0c
Habibov97 <najaff.habibov@gmail.com>
2026-06-30 01:48:47 +0400

    fix

--- src/components/MyTaskContent.tsx
+++ src/components/MyTaskContent.tsx
@@ -12,7 +12,7 @@ export default function MyTaskContent({ myTasks }: { myTasks: TaskType[] }) {
     <section className="px-[76px] flex-1 ">
       <main className="rounded-xl h-[75dvh]">
         <div className="flex max-w-[1000px] xl:max-w-[1400px] justify-center gap-2 h-full">
-          <div className="flex w-1/2 xl:w-[700px] flex-col gap-2 items-center h-full   rounded-xl text-xl pt-[15px] pb-[20px] px-8 border border-stone-200 shadow-[0_0_15px_rgba(0,0,0,0.1)] ">
+          <div className="flex w-1/2 flex-col gap-2 items-center h-full rounded-xl text-xl pt-[15px] pb-[20px] px-8 border border-stone-200 shadow-[0_0_15px_rgba(0,0,0,0.1)] ">
             <div className="self-start mb-2">
               <h2 className="relative text-md font-semibold pb-1 after:absolute after:bottom-0 after:left-1 after:h-[2px] after:w-1/2 after:bg-green-500">
                 My Tasks
